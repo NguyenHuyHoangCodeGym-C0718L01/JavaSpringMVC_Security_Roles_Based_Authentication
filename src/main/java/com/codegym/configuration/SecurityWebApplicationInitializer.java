6c4923e4c96256edcab5c7a139f8e241a551608f
Nguyen Huy Hoang <huyhoang681997@gmail.com>
2019-04-18 09:56:38 +0700

    Hoang finish Java Spring MVC Security Roles Based Authentication project.

---
+++ src/main/java/com/codegym/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,4 @@
+package com.codegym.configuration;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
+}
